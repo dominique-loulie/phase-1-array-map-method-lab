cbf648f2e0a0602eb42644e47d8269527be6cd68
Dominique Loulie <dom.loulie@gmail.com>
2023-05-20 15:49:11 +1000

    this is my first

--- index.js
+++ index.js
@@ -11,27 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
-
-function titleCased() {
-  const tutorials = [
-    "what does the this keyword mean?",
-    "what is the constructor OO pattern?",
-    "implementing Blockchain Web API",
-    "the test driven development workflow",
-    "what is NaN and how can we check for it",
-    "what is the difference between stopPropagation and preventDefault?",
-    "immutable state and pure functions",
-    "what is the difference between == and ===?",
-    "what is the difference between event capturing and bubbling?",
-    "what is JSONP?"
-  ];
-
-  return tutorials.map(function(tutorial) {
-    return tutorial.split(' ').map(function(word) {
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    }).join(' ');
-  });
+function titleCased(){
+  const capTutorials = tutorials.map(t => {
+    const words = t.split(" ").map(w => {
+      return w.charAt(0).toUpperCase() + w.slice(1);
+    }).join(" ");
+    console.log(words);
+    return words; 
+  })
+  return capTutorials;
 }
