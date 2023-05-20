b18c30aadb0583ea82647207380b258b7da2485f
Dominique Loulie <dom.loulie@gmail.com>
2023-05-20 15:19:12 +1000

    this is my first

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+
+function titleCased() {
+  const tutorials = [
+    "what does the this keyword mean?",
+    "what is the constructor OO pattern?",
+    "implementing Blockchain Web API",
+    "the test driven development workflow",
+    "what is NaN and how can we check for it",
+    "what is the difference between stopPropagation and preventDefault?",
+    "immutable state and pure functions",
+    "what is the difference between == and ===?",
+    "what is the difference between event capturing and bubbling?",
+    "what is JSONP?"
+  ];
+
+  return tutorials.map(function(tutorial) {
+    return tutorial.split(' ').map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
+}
